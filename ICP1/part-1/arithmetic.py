810c32d7cd5234d183047a1c2c5d68617235f282
Adavellisahaja <54335540+Adavellisahaja@users.noreply.github.com>
2020-01-24 17:54:57 -0600

    commit

---
+++ ICP1/part-1/arithmetic.py
@@ -0,0 +1,9 @@
+#arithmentic operations on two numbers
+
+import math
+n1 = int(input('Please enter an integer: '))
+n2 = int(input('Please enter a second integer: '))
+print(' sum of two numbers: ', n1+n2)
+print('multiplication of two numbers', n1*n2)
+print('square of first number', n1*n1)
+print('division of two numbers', n1/n2)
