df917ba201801d953d6f2b71dba04fe4c7afc381
Adavellisahaja <54335540+Adavellisahaja@users.noreply.github.com>
2020-02-07 21:37:58 -0600

    commit

---
+++ ICP3/icp3.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+s = np.random.randint(0, 20, 15)
+print(s)
+a= s.reshape((3,5))
+x = a[0]
+x[np.where(x==np.max(x))] = 0
+print(x)
+y = a[1]
+y[np.where(y==np.max(y))] = 0
+print(y)
+z = a[2]
+z[np.where(z==np.max(z))] = 0
+print(z)
+b = [x , y, z]
+print(b)
+
+
+
