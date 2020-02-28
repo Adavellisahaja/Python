0defa25ea73c268e61a4f87fd22a890cc4322c5c
Adavellisahaja <54335540+Adavellisahaja@users.noreply.github.com>
2020-02-28 11:54:31 -0600

    commit

---
+++ ICP6/icp6-1.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style="white", color_codes=True)
+import warnings
+warnings.filterwarnings("ignore")
+
+dataset = pd.read_csv('CC.csv')
+# replacing null values with mean
+dataset = dataset.fillna(dataset.mean())
+x = dataset.iloc[:,1:]
+y = dataset.iloc[:,-1]
+
+
+# featuring
+from sklearn import preprocessing
+scaler =preprocessing.StandardScaler()
+scaler.fit(x)
+x_scaled_array=scaler.transform(x)
+x_scaled=pd.DataFrame(x_scaled_array, columns =x.columns)
+
+# elbow method
+wcss = []
+for i in range(1,10):
+    kmeans = KMeans(n_clusters=i, max_iter=300, random_state=0)
+    kmeans.fit(x_scaled)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1,10),wcss)
+plt.title('the elbow method')
+plt.xlabel('Number of Clusters')
+plt.ylabel('Wcss')
+plt.show()
+
+##building the model
+
+# nclusters = i # this is the k in kmeans
+km = KMeans(n_clusters=2)
+km.fit(x_scaled)
+
+# predict the cluster for each data point
+y_cluster_kmeans = km.predict(x_scaled)
+from sklearn import metrics
+score = metrics.silhouette_score(x_scaled, y_cluster_kmeans)
+print(score)
