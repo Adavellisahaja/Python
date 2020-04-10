a016c84595cee0fd9e06522fc9e72b296fa53411
Adavellisahaja <54335540+Adavellisahaja@users.noreply.github.com>
2020-04-10 18:27:45 -0500

    commit

--- DeepLearning/ICP3/task3.py
+++ DeepLearning/ICP3/task3.py
@@ -27,18 +27,18 @@ max_review_len= max([len(s.split()) for s in sentences])
 vocab_size= len(tokenizer.word_index)+1
 sentences = tokenizer.texts_to_sequences(sentences)
 padded_docs= pad_sequences(sentences,maxlen=max_review_len)
-#sentences = tokenizer.texts_to_matrix(sentences)
+
 
 le = preprocessing.LabelEncoder()
 y = le.fit_transform(y)
 X_train, X_test, y_train, y_test = train_test_split(padded_docs, y, test_size=0.25, random_state=1000)
 
 # Number of features
-# print(input_dim)
+
 model = Sequential()
 model.add(Embedding(vocab_size, 50, input_length=max_review_len))
 model.add(Flatten())
 model.add(layers.Dense(300, activation='relu'))
-model.add(layers.Dense(3, activation='softmax'))
+model.add(layers.Dense(20, activation='softmax'))
 model.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['acc'])
 history=model.fit(X_train,y_train, epochs=2, verbose=True, validation_data=(X_test,y_test), batch_size=256)
